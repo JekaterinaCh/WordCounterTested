dd0681477c3d47ecdabc6f386a3e5eac4a0281c3
JekaterinaCh <jekaterina.chvorostova@gmail.com>
2021-05-16 20:35:49 +0300

    Add tests

--- app/src/main/java/com/example/textcountersample/MainActivity.java
+++ app/src/main/java/com/example/textcountersample/MainActivity.java
@@ -11,34 +11,50 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 import static com.example.textcountersample.utils.ElementsCalculator.getCharsCount;
+import static com.example.textcountersample.utils.ElementsCalculator.getWordsCount;
+import static com.example.textcountersample.utils.ToastMessage.showToast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Spinner ddSelection;
+    Spinner dropdownSelect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.ddSelection = (Spinner) findViewById(R.id.ddSelection);
+        this.dropdownSelect = (Spinner) findViewById(R.id.dropdownSelect);
         ArrayList<String> selectionOptionsList = new ArrayList<>();
-        selectionOptionsList.add("Words");
-        selectionOptionsList.add("Chars");
+        selectionOptionsList.add(this.getString(R.string.words));
+        selectionOptionsList.add(this.getString(R.string.chars));
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, selectionOptionsList);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        this.ddSelection.setAdapter(arrayAdapter);
+        this.dropdownSelect.setAdapter(arrayAdapter);
     }
 
     public void btnCalculateOnClick(View view) {
-        EditText edUserInput = findViewById(R.id.edUserInput);
-        TextView tvOutput = findViewById(R.id.tvOutput);
-        String userInputText = edUserInput.getText().toString();
+        EditText editTextUserInput = findViewById(R.id.editTextUserInput);
+        TextView textViewNumber = findViewById(R.id.textViewNumber);
+        TextView textViewLabel = findViewById(R.id.textViewLabel);
+        String userInputText = editTextUserInput.getText().toString();
 
-        String selection = this.ddSelection.getSelectedItem().toString();
-        if(selection.equalsIgnoreCase("Chars")){
+        String selection = this.dropdownSelect.getSelectedItem().toString();
+        if(selection.equalsIgnoreCase(this.getString(R.string.chars))){
             int count = getCharsCount(userInputText);
-            tvOutput.setText(String.valueOf(count));
+            if(count == 0 ) {
+                showToast(this, this.getString(R.string.empty)
+                );
+            }
+            textViewLabel.setText(this.getString(R.string.chars_count));
+            textViewNumber.setText(String.valueOf(count));
+        }
+        else{
+            int count = getWordsCount(userInputText);
+            if(count == 0 ) {
+                showToast(this,  this.getString(R.string.empty));
+            }
+            textViewLabel.setText(this.getString(R.string.words_count));
+            textViewNumber.setText(String.valueOf(count));
         }
     }
 }
--- app/src/main/java/com/example/textcountersample/utils/ElementsCalculator.java
+++ app/src/main/java/com/example/textcountersample/utils/ElementsCalculator.java
@@ -1,7 +1,26 @@
 package com.example.textcountersample.utils;
 
 public class ElementsCalculator {
-    public static int getCharsCount(String inputText){
-        return inputText.length();
+    public static int getCharsCount(String str){
+        return str.length();}
+
+    public static int getWordsCount(String str){
+        if(str.length() == 0)
+            return 0;
+
+        int count =0;
+        for(int i=0;i< str.length();i++) {
+
+            if(str.charAt(i) == ' ')
+                continue;
+
+            if (i > 0 && str.charAt(i - 1) == ' ') {
+                count++;
+            } else if (i == 0 && str.charAt(i) != ' ') {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
